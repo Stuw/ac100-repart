2df9d6975312c5b91a4c06bd5837430002a99bf9
Andrey Danin <danindrey@mail.ru>
2013-01-14 18:10:20 +0400

    Added get_raw_data to nvflash module

--- modules/nvflash.py
+++ modules/nvflash.py
@@ -83,6 +83,10 @@ def push_part(id, file):
 	return execute('sudo "%s" -r --download %d "%s" --sync' % (nvflash(), id, file))
 
 
+def get_raw_data(start, size, file):
+	return execute('sudo "%s" -r --rawdeviceread %d %d "%s" --sync"' % (nvflash(), start, size, file))
+
+
 if __name__ == '__main__': 
 	ret = init()
 	print "init executed with result %i" % ret
